51504695a929ba539894bc532b13eae2a4514064
Matthieu Keller <keller.mdpa@gmail.com>
2015-02-25 23:59:28 +0100

    previous code was a bit bad :D

--- lorem_ipsum.py
+++ lorem_ipsum.py
@@ -5,25 +5,23 @@ import random
 
 
 def main():
-    pass
+    # open all files contain in locations
+    locations = open('locations', 'r')
+    words = []
+    for location in locations:
+        if os.path.exists(location.strip()):
+            try:
+                for filename in os.listdir(location.strip()):
+                    f = open(location.strip()+filename, 'r')
+                    for line in f:
+                        words.append(line.strip())
+            except FileNotFoundError:
+                pass
 
-# open all files contain in locations
-locations = open('locations', 'r')
-words = []
-for location in locations:
-    if os.path.exists(location.strip()):
-        try:
-            for filename in os.listdir(location.strip()):
-                f = open(location.strip()+filename, 'r')
-                for line in f:
-                    words.append(line.strip())
-        except FileNotFoundError:
-            pass
+    # sort all the words ? (if not to slow)
 
-# sort all the words ? (if not to slow)
-
-# print random
-print(words[random.randint(0, len(words)-1)])
+    # print random
+    print(words[random.randint(0, len(words)-1)])
 
 if __name__ == "__main__":
     main()

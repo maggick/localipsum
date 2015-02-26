31a62bafe24fbbfbfb280b72f02f1c494f629412
Matthieu Keller <keller.mdpa@gmail.com>
2015-02-26 22:59:49 +0100

    add printSentences function, add lorem ipsum start

--- lorem_ipsum.py
+++ lorem_ipsum.py
@@ -20,23 +20,34 @@ def main():
 
     # sort all the words ? (if not to slow)
 
+    str = "Lorem ipsum dolor sit amet, "
+
     # print random
-    # print(printWords(words, 5))
-    print(printParagraphs(words, 5))
+    # str += printWords(words, 5)
+    str += printParagraphs(words, 5)
+    print(str)
 
 
 def printWords(words, n):
+    # if n > ? : let's add some comma
     str = ""
     for i in range(0, n-1):
         str += words[random.randint(0, len(words)-1)]+" "
-    return str + words[random.randint(0, len(words)-1)]
+    return (str + words[random.randint(0, len(words)-1)])
+
+
+def printSentences(words, n):
+    str = ""
+    for i in range(0, n-1):
+        str += (printWords(words, random.randint(10, 25))+". ").capitalize()
+    return str + (printWords(words, random.randint(10, 25))+".").capitalize()
 
 
 def printParagraphs(words, n):
     str = ""
     for i in range(0, n-1):
-        str += printWords(words, random.randint(25, 45))+".\n\n"
-    return str + printWords(words, random.randint(25, 45))+"."
+        str += printSentences(words, random.randint(4, 8))+"\n\n"
+    return str + printSentences(words, random.randint(4, 8))
 
 
 if __name__ == "__main__":
